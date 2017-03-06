77b6467a7109cfc14981ac2fedc22e9c4a922f63
Mister4Eyes <mister4eyez@gmail.com>
2017-03-06 17:05:01 -0500

    Fixed some bullshit.

--- StartServer.bat
+++ StartServer.bat
@@ -1,1 +1,7 @@
+@echo off
+echo Copying frontend files to server.
+
+xcopy "RTF2L-Frontend" "RTF2L-Backend\RandomTF2Loadout\bin\Release" /O /X /E /H /K
+
+echo Starting Server
 start RTF2L-Backend\RandomTF2Loadout\bin\Release\RandomTF2Loadout.exe
