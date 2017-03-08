a26694dd8f1d16a5c53915aaf44345193b8b2b96
Mister4Eyes <mister4eyez@gmail.com>
2017-03-08 00:02:51 -0500

    Fixed some english in the prebuild script.

--- RTF2L-Backend/scripts/PreBuild.bat
+++ RTF2L-Backend/scripts/PreBuild.bat
@@ -1,6 +1,5 @@
 @echo off
-
 cd "..\..\..\.."
 
-echo Copying configuration file to %1 folder.
+echo Copying configuration file to %1.
 copy "config.cfg" "RTF2L-Backend\RandomTF2Loadout\bin\%1\config.cfg"
